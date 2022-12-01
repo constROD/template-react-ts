9463f2a4ff455ecd2f76c2c2b033de43bfff2c56
constROD <rialcantara21@gmail.com>
2022-12-02 03:00:52 +0800

    chore: update vscode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import './global.css';
 import App from './App';
+import './global.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
--- src/shared/components/AuthenticatedRoute.tsx
+++ src/shared/components/AuthenticatedRoute.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { ROUTES } from 'shared/constants/Routes';
 import { useUserStore } from 'shared/store';
 
