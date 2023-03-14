83100626ae9790c2c089d9b3784de2a44ffa08cd
constROD <rialcantara21@gmail.com>
2023-03-15 01:35:08 +0800

    chore: update environments comment

--- src/shared/constants/environments.ts
+++ src/shared/constants/environments.ts
@@ -1,7 +1,6 @@
 /**
  * * When you add a new environment, you must add it to the list below.
- * * You must also add it to the list of environments in the .env file.
- * * You must also add it to the next.config.js file.
+ * * - You must also add it to the list of environments in the .env file.
  */
 
 export const STAGES = {
