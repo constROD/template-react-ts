0b78020f2deed340694bdf5d16f2f49a82f6d07e
constROD <rialcantara21@gmail.com>
2023-03-15 02:41:05 +0800

    chore: update env comments

--- src/shared/constants/environments.ts
+++ src/shared/constants/environments.ts
@@ -1,6 +1,8 @@
 /**
  * * When you add a new environment, you must add it to the list below.
- * * - You must also add it to the list of environments in the .env file.
+ * * - You must add it to the `.env` file.
+ * * - You must add it to the `.env.example` file.
+ * * - You must add it to the `src/shared/constants/environments.ts` file.
  */
 
 export const STAGES = {
